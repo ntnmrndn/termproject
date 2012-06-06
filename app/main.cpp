9832494ac421f870c50c1070721bdbf4ee67788f
GregTheRules <gbarry.epitech@gmail.com>
2012-06-06 16:44:27 -0700

    Update du parseur XML Beta

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,18 @@
 #include <QApplication>
+#include <qdebug.h>
 #include "mainwindow.h"
+#include "ParseXml.h"
 
 int  main(int argc, char *argv[])
 {
-  QApplication app(argc, argv);
+	qDebug() << "toto";
+	QApplication app(argc, argv);
+	ParseXml test;
 
-  app.setOrganizationName("EPITECH");
-  app.setApplicationName("Term project");
-  MainWindow main_Window;
-  main_Window.show();
-
-  return (app.exec());
+	app.setOrganizationName("EPITECH");
+	app.setApplicationName("Term project");
+	//MainWindow main_Window;
+	//main_Window.show();
+	test.readFile();
+	return (app.exec());
 }
